a49ec813736e97f5f27724a37929c11814b18d46
wendell <wendell_xw20@hotmail.com>
2023-12-27 11:23:30 -0300

    commit

---
+++ obj.py
@@ -0,0 +1,26 @@
+from random import randint
+class Pessoa:
+    ano_atual = 2023
+
+    def __init__(self, nome, idade):
+        self. nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        print(self.ano_atual - self.idade)
+
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimento):
+        idade = cls.ano_atual - ano_nascimento
+        return cls(nome, idade)
+    
+    @staticmethod
+    def gerar_id():
+        rand = randint(10000, 19999)
+        return rand
+    
+
+p1 = Pessoa.por_ano_nascimento('wendell', 1996)
+print(p1.nome, p1.idade)
+p1.get_ano_nascimento()
+print(f'seu id: {Pessoa.gerar_id()}')
